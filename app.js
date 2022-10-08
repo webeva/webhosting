898a998580aa55cb81f01a175d3736171f290e6f
unknown <web3eva@gmail.com>
2022-10-08 07:14:23 +0100

    change

--- app.js
+++ app.js
@@ -8,42 +8,46 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-
-
 async function getuserinfo(request){
- 
     const result = await deso.getKey(request)
     return result
 }
 
 app.get('*', (req, res) => {
-  var dir = `./${req.originalUrl.substring(1)}`;
-  if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-  }
+  
   const value = Promise.resolve(getuserinfo(req.originalUrl.substring(1))).then(result =>{
     if(result){
-      const res = Object.values(JSON.parse(result))
-      for (let i = 0; i < res.length; i++) {
-        fs.writeFile(`./${req.originalUrl.substring(1)}/${Object.keys(res[i])[0]}`,`${Object.values(res[i])[0]}`, function(err){
+      var dir = `./${req.originalUrl.substring(1)}`;
+      if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+      }
+      const resp = Object.values(JSON.parse(result))
+      for (let i = 0; i < resp.length; i++) {
+      
+      
+        fs.writeFile(`./${req.originalUrl.substring(1)}/${Object.keys(resp[i])[0]}`,`${Object.values(resp[i])[0]}`, function(err){
           console.log("Saved")
         }) 
       }
+    }else{
+      res.sendFile(path.resolve() + `/error.html`)
     }
     
   })
+  
   try{
-    res.sendFile(path.resolve() + `/${req.originalUrl.substring(1)}/index.html`)
+    
+      if(fs.existsSync(path.resolve() + `/${req.originalUrl.substring(1)}`)){
+        res.sendFile(path.resolve() + `/${req.originalUrl.substring(1)}/index.html`)
+      }else{
+        res.sendFile(path.resolve() + `/error.html`)
+      }
   }catch(err){
     res.sendFile("If you are expecting a website refresh the page.")
-  }    
+  }   
 })
 
 
 app.listen(port, () => {
-  /*
-  */
-  
-    
- 
+  console.log("Listening of this path")
 })
